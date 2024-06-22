9a0057defa676ec976e0168961370fa235650e24
Cruzer-S <yyyynoom@gmail.com>
2024-06-22 07:06:24 -0400

    first commit

---
+++ include/termcolor.h
@@ -0,0 +1,39 @@
+#ifndef TERM_COLOR_H__
+#define TERM_COLOR_H__
+
+#define fDefault	\033[39m
+#define bDefault	\033[49m
+#define fBlack		\033[30m
+#define bBlack		\033[40m
+#define fDarkRed	\033[31m
+#define bDarkRed	\033[41m
+#define fDarkGreen	\033[32m
+#define bDarkGreen	\033[42m
+#define fDarkYellow 	\033[33m
+#define bDarkYellow	\033[43m
+#define fDarkBlue	\033[34m
+#define bDarkBlue	\033[44m
+#define fDarkMagenta	\033[35m
+#define bDarkMagenta	\033[45m
+#define fDarkCyan	\033[36m
+#define bDarkCyan	\033[46m
+#define fLightGray	\033[37m
+#define bLightGray	\033[47m
+#define fDarkGray	\033[90m
+#define bDarkGray	\033[100m
+#define fRed		\033[91m
+#define bRed		\033[101m
+#define fGreen		\033[92m
+#define bGreen		\033[101m
+#define fOrange		\033[93m
+#define bOrange 	\033[103m
+#define fBlue		\033[94m
+#define bBlue		\033[104m
+#define fMagenta	\033[95m
+#define bMangenta	\033[105m
+#define fCyan		\033[96m
+#define bCyan		\033[106m
+#define fWhite		\033[97m
+#define bWhite		\033[107m
+
+#endif
